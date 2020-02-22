282a7015b16a4e4a2345b3a7546ef118c23de810
Kazymir Rabier <k.rabier@ncl.ac.uk>
2020-02-22 20:12:55 +0000

    Started project from scratch, added navigation drawer with a few items

---
+++ app/src/main/java/com/kazymir/tripweaver/activity/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.kazymir.tripweaver.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.kazymir.tripweaver.R
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawer: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
---
+++ app/src/main/java/com/kazymir/tripweaver/object/Trip.kt
@@ -0,0 +1,8 @@
+package com.kazymir.tripweaver.`object`
+
+import java.util.*
+
+class Trip(var title: String, var startDate: Date, var endDate: Date, val destName: String) {
+//    val durationInDays =
+
+}
